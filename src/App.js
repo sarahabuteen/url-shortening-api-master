2d880eaf54b05f12526610c3535550868b7fe9bd
Sarah Abuteen <sabuteen@terkwaz.com>
2020-08-23 01:33:56 +0300

    Footer section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "./components/shared/Header";
 import Home from "./components/Home";
+import Footer from "./components/shared/Footer";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
       <Home />
+      <Footer />
     </React.Fragment>
   );
 }
---
+++ src/components/shared/Footer.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import logo from "../../assets/images/logo-white.svg";
+import facebook from "../../assets/images/icon-facebook.svg";
+import twitter from "../../assets/images/icon-twitter.svg";
+import pinterest from "../../assets/images/icon-pinterest.svg";
+import instagram from "../../assets/images/icon-instagram.svg";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <section className="footer">
+        <Container>
+          <Row className="d-flex align-items-center">
+            <Col md={4}>
+              <img src={logo} alt="logo" />
+            </Col>
+            <Col md={2}>
+              <h6 className="text-white mb-0">Features</h6>
+            </Col>
+            <Col md={2}>
+              <h6 className="text-white mb-0">Resources</h6>
+            </Col>
+            <Col md={2}>
+              <h6 className="text-white mb-0">Company</h6>
+            </Col>
+            <Col md={2}>
+              <ul className="list-unstyled list-inline mb-0 w-100 d-inline-flex justify-content-around">
+                <li className="list-inline-item">
+                  <img src={facebook} alt="facebook" />
+                </li>
+                <li className="list-inline-item">
+                  <img src={twitter} alt="twitter" />
+                </li>
+                <li className="list-inline-item">
+                  <img src={pinterest} alt="pinterest" />
+                </li>
+                <li className="list-inline-item">
+                  <img src={instagram} alt="instagram" />
+                </li>
+              </ul>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={4} />
+            <Col md={2}>
+              <ul className="list-unstyled mb-0 mt-3">
+                <li className="grayish-violet-text mb-2">Link Shortening</li>
+                <li className="grayish-violet-text mb-2">Branded Links</li>
+                <li className="grayish-violet-text">Analytics</li>
+              </ul>
+            </Col>
+            <Col md={2}>
+              <ul className="list-unstyled mb-0 mt-3">
+                <li className="grayish-violet-text mb-2">Blog</li>
+                <li className="grayish-violet-text mb-2">Developers</li>
+                <li className="grayish-violet-text">Support</li>
+              </ul>
+            </Col>
+            <Col md={2}>
+              <ul className="list-unstyled mb-0 mt-3">
+                <li className="grayish-violet-text mb-2">About</li>
+                <li className="grayish-violet-text mb-2">Our team</li>
+                <li className="grayish-violet-text mb-2">Careers</li>
+                <li className="grayish-violet-text">Contact</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    );
+  }
+}
