cebda1c3367bbf17ca5f6c5365d5cfe2fb40b3c8
Sarah Abuteen <sabuteen@terkwaz.com>
2020-08-23 01:02:33 +0300

    Statistics section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 
 import illustration from "../assets/images/illustration-working.svg";
-
+import brandRecognition from "../assets/images/icon-brand-recognition.svg";
+import detailedRecords from "../assets/images/icon-detailed-records.svg";
+import fullyCustomizable from "../assets/images/icon-fully-customizable.svg";
 export default class Home extends Component {
   render() {
     return (
@@ -20,6 +22,60 @@ export default class Home extends Component {
               </Col>
               <Col md={6}>
                 <img src={illustration} alt="illustration" className="ml-5" />
+              </Col>
+            </Row>
+          </Container>
+        </section>
+
+        <section className="statistics">
+          <Container>
+            <Row>
+              <Col md={12} className="d-flex flex-column align-items-center">
+                <h2 className="dark-blue-text">Advanced Statistics</h2>
+                <p className="grayish-violet-text subtitle">
+                  Track how your links are performing across the web with our
+                  advanced statistics dashboard.
+                </p>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={4}>
+                <div className="statistics-box">
+                  <div className="icon">
+                    <img src={brandRecognition} alt="brand recognition" />
+                  </div>
+                  <h4 className="dark-blue-text">Brand Recognition</h4>
+                  <p className="grayish-violet-text">
+                    Check your brand recognition with each click. Generic links
+                    don't mean a thing. Branded links help instil confidence in
+                    your content.
+                  </p>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className="statistics-box middle">
+                  <div className="icon">
+                    <img src={detailedRecords} alt="detailed records" />
+                  </div>
+                  <h4 className="dark-blue-text">Detailed Records</h4>
+                  <p className="grayish-violet-text">
+                    Gain insights into who is clicking your links. Knowing when
+                    and where people engage with your content helps inform
+                    better descisions.
+                  </p>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className="statistics-box last">
+                  <div className="icon">
+                    <img src={fullyCustomizable} alt="fully customizable" />
+                  </div>
+                  <h4 className="dark-blue-text">Fully Customizable</h4>
+                  <p className="grayish-violet-text">
+                    Improve brand awareness and content discoverability through
+                    customizable links, supercharging audience engagement.
+                  </p>
+                </div>
               </Col>
             </Row>
           </Container>
